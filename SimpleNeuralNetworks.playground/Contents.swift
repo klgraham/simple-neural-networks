27af0614df7e08d991f34ec7a9c05cd62b4b435a
Kenneth Graham <klgraham@gmail.com>
2016-04-03 19:15:46 -0500

    Added training and evaluation

--- SimpleNeuralNetworks.playground/Contents.swift
+++ SimpleNeuralNetworks.playground/Contents.swift
@@ -1,28 +1,11 @@
-//: Playground - noun: a place where people can play
-
-import UIKit
+import Foundation
 
 // Perceptron
 
-let bias = 1.0
-var biasWeight = 1.0
-var x: [Double] = [12, 4]
-var w: [Double] = [0.5, -1]
 
-func step(z: Double) -> Int {
-    return (z >= 0) ? 1 : 0
+func randomDouble() -> Double {
+    return Double(arc4random()) / Double(UINT32_MAX)
 }
-
-func activate(bias: Double, bW: Double, x: [Double], w: [Double]) -> Int {
-    var sum = 0.0
-    for i in 0..<x.count {
-        sum += x[i] * w[i]
-    }
-    sum += bias * bW
-    return step(sum)
-}
-
-activate(bias, bW: biasWeight, x: x, w: w)
 
 struct Perceptron {
     var bias: Double
@@ -43,13 +26,16 @@ struct Perceptron {
         return (sum > 0) ? 1 : offState
     }
     
-    mutating func backProp(input: [Double], output: Int) {
+    mutating func backProp(input: [Double], output: Int) -> Int {
         let prediction = feedForward(input)
         let error = output - prediction
         
-        for i in 0..<weights.count {
-            weights[i] += learningRate * Double(error) * input[i]
+        for i in 0..<input.count {
+            weights[i + 1] += learningRate * Double(error) * input[i]
         }
+        weights[0] += learningRate * Double(error) * bias
+        
+        return error
     }
     
     init(numInputs: Int, offState: Int, bias: Double) {
@@ -58,7 +44,7 @@ struct Perceptron {
         
         self.weights = [Double]()
         for _ in 0...numInputs {
-            weights.append(1.0 * (drand48() - 0.5))
+            weights.append(1.0 * (randomDouble() - 0.5))
         }
     }
     
@@ -67,10 +53,83 @@ struct Perceptron {
         
         self.weights = [Double]()
         for _ in 0...numInputs{
-            weights.append(1.0 * (drand48() - 0.5))
+            weights.append(1.0 * (randomDouble() - 0.5))
         }
     }
 }
 
-var p = Perceptron(numInputs: 2, offState: -1, bias: 1)
-p.feedForward(x)
+
+// can we predict if a given x value is above or below the line y=2x+1 ?
+var line = { (x: Double) in return 3 * x + 1 }
+
+var isAbove = { (x: Double) in return line(x) > 0 ? 1 : -1 }
+
+var p = Perceptron(numInputs: 1, offState: -1, bias: 1)
+
+struct PerceptronDataPair {
+    let input: [Double]
+    let output: Int
+}
+
+struct PerceptronTrainer {
+    let data: [PerceptronDataPair]
+    
+    func train(inout p: Perceptron) -> Int {
+        var error: Int = 0
+        var count = 0
+        
+        for d in data {
+            error = p.backProp(d.input, output: d.output)
+            
+//            if (count % 5 == 0) {
+//                print("Iter: \(count), Error: \(error)")
+//            }
+            count += 1
+        }
+        
+        return error
+    }
+}
+
+// create training data
+func createData(numPoints: Int) -> [PerceptronDataPair] {
+    var data = [PerceptronDataPair]()
+    
+    for _ in 0..<numPoints {
+        let x = [2.0 * (randomDouble() - 0.5)]
+        let y = line(x[0])
+        data.append(PerceptronDataPair(input: x, output: isAbove(y)))
+    }
+
+    return data
+}
+
+let trainingData = createData(100)
+
+
+let trainer = PerceptronTrainer(data: trainingData)
+trainer.train(&p)
+
+// let's see how good it works
+
+let testData = createData(100)
+
+func evaluatePerceptron(p: Perceptron, testData: [PerceptronDataPair]) -> Double {
+    var correct = 0
+    for d in testData {
+        let prediction = p.feedForward(d.input)
+        if (prediction == d.output) {
+            correct += 1
+        }
+    }
+    
+    return Double(correct) / Double(testData.count)
+}
+
+evaluatePerceptron(p, testData: testData)
+
+let pUntrained = Perceptron(numInputs: 1, offState: -1, bias: 1)
+evaluatePerceptron(pUntrained, testData: testData)
+
+
+
