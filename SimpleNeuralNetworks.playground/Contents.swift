f6c3692deb56ef351401ba1b902f2314c5e61f65
Kenneth Graham <klgraham@gmail.com>
2017-06-05 23:39:42 -0500

    Changed Perceptron from a struct to a class since the weights are frequently mutated during training.

--- SimpleNeuralNetworks.playground/Contents.swift
+++ SimpleNeuralNetworks.playground/Contents.swift
@@ -7,7 +7,7 @@ func randomDouble() -> Double {
     return Double(arc4random()) / Double(UINT32_MAX)
 }
 
-struct Perceptron {
+class Perceptron {
     var bias: Double
     private var offState: Int = 0
     let learningRate = 0.01
@@ -15,28 +15,6 @@ struct Perceptron {
     // weights[0] is the weight for the bias input
     var weights: [Double]
     
-    func feedForward(input: [Double]) -> Int {
-        assert(input.count + 1 == weights.count)
-        
-        var sum = 0.0
-        for i in 0..<input.count {
-            sum += input[i] * weights[i + 1]
-        }
-        sum += bias * weights[0]
-        return (sum > 0) ? 1 : offState
-    }
-    
-    mutating func backProp(input: [Double], output: Int) -> Int {
-        let prediction = feedForward(input)
-        let error = output - prediction
-        
-        for i in 0..<input.count {
-            weights[i + 1] += learningRate * Double(error) * input[i]
-        }
-        weights[0] += learningRate * Double(error) * bias
-        
-        return error
-    }
     
     init(numInputs: Int, offState: Int, bias: Double) {
         self.offState = offState
@@ -56,6 +34,29 @@ struct Perceptron {
             weights.append(1.0 * (randomDouble() - 0.5))
         }
     }
+    
+    func feedForward(input: [Double]) -> Int {
+        assert(input.count + 1 == weights.count)
+        
+        var sum = 0.0
+        for i in 0..<input.count {
+            sum += input[i] * weights[i + 1]
+        }
+        sum += bias * weights[0]
+        return (sum > 0) ? 1 : offState
+    }
+    
+    func backProp(input: [Double], output: Int) -> Int {
+        let prediction = feedForward(input: input)
+        let error = output - prediction
+        
+        for i in 0..<input.count {
+            weights[i + 1] += learningRate * Double(error) * input[i]
+        }
+        weights[0] += learningRate * Double(error) * bias
+        
+        return error
+    }
 }
 
 
@@ -74,16 +75,16 @@ struct PerceptronDataPair {
 struct PerceptronTrainer {
     let data: [PerceptronDataPair]
     
-    func train(inout p: Perceptron) -> Int {
+    func train( p: inout Perceptron) -> Int {
         var error: Int = 0
         var count = 0
         
         for d in data {
-            error = p.backProp(d.input, output: d.output)
+            error = p.backProp(input: d.input, output: d.output)
             
-//            if (count % 5 == 0) {
-//                print("Iter: \(count), Error: \(error)")
-//            }
+            if (count % 5 == 0) {
+                print("Iter: \(count), Error: \(error)")
+            }
             count += 1
         }
         
@@ -104,20 +105,20 @@ func createData(numPoints: Int) -> [PerceptronDataPair] {
     return data
 }
 
-let trainingData = createData(100)
+let trainingData = createData(numPoints: 100)
 
 
 let trainer = PerceptronTrainer(data: trainingData)
-trainer.train(&p)
+trainer.train(p: &p)
 
 // let's see how good it works
 
-let testData = createData(100)
+let testData = createData(numPoints: 100)
 
 func evaluatePerceptron(p: Perceptron, testData: [PerceptronDataPair]) -> Double {
     var correct = 0
     for d in testData {
-        let prediction = p.feedForward(d.input)
+        let prediction = p.feedForward(input: d.input)
         if (prediction == d.output) {
             correct += 1
         }
@@ -127,10 +128,10 @@ func evaluatePerceptron(p: Perceptron, testData: [PerceptronDataPair]) -> Double
 }
 
 // The % correct will be much higher than for an untrained perceptron
-evaluatePerceptron(p, testData: testData)
+evaluatePerceptron(p: p, testData: testData)
 
 let pUntrained = Perceptron(numInputs: 1, offState: -1, bias: 1)
-evaluatePerceptron(pUntrained, testData: testData)
+evaluatePerceptron(p: pUntrained, testData: testData)
 
 
 
